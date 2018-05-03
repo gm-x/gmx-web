d4464ceee75ec687058427a8fc6855cd38243ef1
fant1kua <fant1kua@users.noreply.github.com>
2018-05-04 00:44:19 +0300

    Database

--- index.php
+++ index.php
@@ -9,9 +9,9 @@ $container = $app->getContainer();
 
 $container['root'] = __DIR__ . DIRECTORY_SEPARATOR;
 
-$container['view'] = function ($c) {
-    $view = new \Slim\Views\Twig($c->get('root') . 'templates', [
-        'cache' => $c->get('root') . 'cache',
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig($container->get('root') . 'templates', [
+        'cache' => $container->get('root') . 'cache',
         // DEBUG
         'debug' => true,
         'auto_reload' => true,
@@ -19,10 +19,29 @@ $container['view'] = function ($c) {
     ]);
 
     // Instantiate and add Slim specific extension
-    $basePath = rtrim(str_ireplace('index.php', '', $c->get('request')->getUri()->getBasePath()), '/');
-    $view->addExtension(new Slim\Views\TwigExtension($c->get('router'), $basePath));
+    $basePath = rtrim(str_ireplace('index.php', '', $container->get('request')->getUri()->getBasePath()), '/');
+    $view->addExtension(new Slim\Views\TwigExtension($container->get('router'), $basePath));
 
     return $view;
+};
+
+$container['db'] = function ($container) {
+    $capsule = new \Illuminate\Database\Capsule\Manager;
+    $capsule->addConnection([
+        'driver' => 'mysql',
+        'host' => '127.0.0.1',
+        'database' => 'test',
+        'username' => 'root',
+        'password' => '',
+        'charset'   => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix'    => '',
+    ]);
+
+    $capsule->setAsGlobal();
+    $capsule->bootEloquent();
+
+    return $capsule;
 };
 
 include __DIR__ . '/src/routes.php';
