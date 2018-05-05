9078f485c88abe249ce2133e1e7945fb784b482e
s008nyx <aleksey87@mail.ru>
2018-05-06 00:16:20 +0300

    JSON config comeback))

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-$config = require __DIR__ . '/src/config.php';
+$config = json_decode(file_get_contents(__DIR__ . '/config.json'), true);
 $app = new \Slim\App($config);
 
 include __DIR__ . '/src/dependencies.php';
