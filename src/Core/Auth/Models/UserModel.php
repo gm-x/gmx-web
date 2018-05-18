f5ace383900c6055af13d9af07f4b080b7aeccbe
fant1kua <fant1kua@users.noreply.github.com>
2018-05-18 18:15:19 +0300

    UserModel

--- src/Core/Auth/Models/UserModel.php
+++ src/Core/Auth/Models/UserModel.php
@@ -1,9 +1,14 @@
 <?php
 namespace GameX\Core\Auth\Models;
 
-use \Cartalyst\Sentinel\Users\EloquentUser;
+use \Illuminate\Database\Eloquent\Model;
 use \GameX\Core\Auth\SentinelBootstrapper;
 use \Cartalyst\Sentinel\Permissions\StrictPermissions;
+use \Cartalyst\Sentinel\Persistences\EloquentPersistence;
+use \Cartalyst\Sentinel\Users\UserInterface;
+use \Cartalyst\Sentinel\Persistences\PersistableInterface;
+use \Cartalyst\Sentinel\Permissions\PermissibleInterface;
+use \Cartalyst\Sentinel\Permissions\PermissibleTrait;
 
 /**
  * Class UserModel
@@ -17,19 +22,110 @@ use \Cartalyst\Sentinel\Permissions\StrictPermissions;
  * @property \DateTime $created_at
  * @property \DateTime $update_at
  */
-class UserModel extends EloquentUser {
+class UserModel extends Model implements UserInterface, PersistableInterface, PermissibleInterface {
+
+	use PermissibleTrait;
+
+	protected $table = 'users';
+
+	/**
+	 * {@inheritDoc}
+	 */
 	protected $fillable = [
 		'email',
 		'password',
 		'last_name',
 		'first_name',
 		'permissions',
-		'role_id'
+		'role_id',
 	];
 
-	// TODO: Make refactoring for it
-	public function roles() {
-		return parent::roles();
+	/**
+	 * {@inheritDoc}
+	 */
+	protected $hidden = [
+		'password',
+	];
+
+	/**
+	 * Returns the user primary key.
+	 *
+	 * @return int
+	 */
+	public function getUserId() {
+		return $this->id;
+	}
+
+	/**
+	 * Returns the user login.
+	 *
+	 * @return string
+	 */
+	public function getUserLogin() {
+		return $this->email;
+	}
+
+	/**
+	 * Returns the user login attribute name.
+	 *
+	 * @return string
+	 */
+	public function getUserLoginName() {
+		return 'email';
+	}
+
+	/**
+	 * Returns the user password.
+	 *
+	 * @return string
+	 */
+	public function getUserPassword() {
+		return $this->password;
+	}
+
+	/**
+	 * Returns the persistable key name.
+	 *
+	 * @return string
+	 */
+	public function getPersistableKey() {
+		return 'user_id';
+	}
+
+	/**
+	 * Returns the persistable key value.
+	 *
+	 * @return string
+	 */
+	public function getPersistableId() {
+		return $this->id;
+	}
+
+	/**
+	 * Returns the persistable relationship name.
+	 *
+	 * @return string
+	 */
+	public function getPersistableRelationship() {
+		return 'persistences';
+	}
+
+	/**
+	 * Generates a random persist code.
+	 *
+	 * @return string
+	 */
+	public function generatePersistenceCode() {
+		return str_random(32);
+	}
+
+	/**
+	 * Returns the persistences relationship.
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\HasMany
+	 */
+	public function persistences() {
+		return $this->hasMany(EloquentPersistence::class, 'user_id');
 	}
 
 	/**
@@ -39,9 +135,24 @@ class UserModel extends EloquentUser {
 		return $this->belongsTo(SentinelBootstrapper::ROLE_MODEL)->first();
 	}
 
-	// TODO: Make refactoring for it
-	public function inRole($role) {
-		return parent::inRole($role);
+	/**
+	 * Returns if access is available for all given permissions.
+	 *
+	 * @param  array|string  $permissions
+	 * @return bool
+	 */
+	public function hasAccess($permissions) {
+		return $this->getPermissionsInstance()->hasAccess($permissions);
+	}
+
+	/**
+	 * Returns if access is available for any given permissions.
+	 *
+	 * @param  array|string  $permissions
+	 * @return bool
+	 */
+	public function hasAnyAccess($permissions) {
+		return $this->getPermissionsInstance()->hasAnyAccess($permissions);
 	}
 
 	/**
