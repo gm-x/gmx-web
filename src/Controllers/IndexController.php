bf478eb360fa8d898518e4f65814aa11fda35d16
fant1kua <fant1kua@users.noreply.github.com>
2018-06-04 14:26:29 +0300

    Monitoring

--- src/Controllers/IndexController.php
+++ src/Controllers/IndexController.php
@@ -2,23 +2,42 @@
 namespace GameX\Controllers;
 
 use \GameX\Core\BaseController;
+use GameX\Models\Server;
 use \Psr\Http\Message\RequestInterface;
 use \Psr\Http\Message\ResponseInterface;
 
 class IndexController extends BaseController {
     public function indexAction(RequestInterface $request, ResponseInterface $response, array $args) {
-//    	/** @var \Stash\Pool $item$cache */
-//		$cache = $this->getContainer('cache');
-//		$cache->setLogger($this->getContainer('log'));
-//		$item = $cache->getItem('test');
-//		$data = $item->get();
-//		if ($item->isMiss()) {
-//			$item->lock();
-//			$data = date('d.m.Y H:i:s');
-//			$item->set($data);
-//			$item->expiresAfter(60);
-//			$cache->save($item);
-//		}
-        return $this->render('index/index.twig');
+    	/** @var \Stash\Pool $item$cache */
+		$cache = $this->getContainer('cache');
+
+		$servers = [];
+		/** @var Server $server */
+		foreach (Server::all() as $server) {
+			$item = $cache->getItem('server_' . $server->id);
+			if ($item->isMiss()) {
+				$servers[] = [
+					'success' => false,
+					'name' => $server->name,
+					'ip' => $server->ip,
+					'port' => $server->port,
+				];
+			} else {
+				$data = $item->get();
+				$servers[] = [
+					'success' => true,
+					'name' => $server->name,
+					'ip' => $server->ip,
+					'port' => $server->port,
+					'map' => $data['map'],
+					'players' => $data['players'],
+					'maxPlayers' => $data['maxPlayers'],
+				];
+			}
+		}
+
+        return $this->render('index/index.twig', [
+        	'servers' => $servers,
+		]);
     }
 }
