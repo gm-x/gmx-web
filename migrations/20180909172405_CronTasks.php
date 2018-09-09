c533ad024d532dd15077fb718afd33354ec6b3cf
fant1kua <fant1kua@users.noreply.github.com>
2018-09-09 17:35:09 +0300

    Cron refactoring

---
+++ migrations/20180909172405_CronTasks.php
@@ -0,0 +1,42 @@
+<?php
+use \GameX\Core\Migration;
+use \GameX\Models\Task;
+
+class CronTasks extends Migration {
+    
+    /**
+     * Do the migration
+     */
+    public function up() {
+        foreach ($this->getList() as $item) {
+            Task::create($item);
+        }
+    }
+    
+    /**
+     * Undo the migration
+     */
+    public function down() {
+        foreach ($this->getList() as $item) {
+            Task::where($item)->delete();
+        }
+    }
+
+    /**
+     * @return array
+     */
+    protected function getList() {
+        return [
+            [
+                'key' => 'punishments',
+                'data' => json_encode([]),
+                'execute_at' => 0,
+                'status' => Task::STATUS_WAITING,
+                'retries' => 0,
+                'max_retries' => 3,
+            ]
+        ];
+    }
+}
+
+
