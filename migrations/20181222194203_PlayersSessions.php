2b7789aaabbbee50c80ce6294d146f47cfc85395
fant1kua <fant1kua@users.noreply.github.com>
2018-12-22 21:17:25 +0200

    Create migration for sessions

---
+++ migrations/20181222194203_PlayersSessions.php
@@ -0,0 +1,38 @@
+<?php
+use \GameX\Core\Migration;
+use \Illuminate\Database\Schema\Blueprint;
+
+class PlayersSessions extends Migration {
+    
+    /**
+     * Do the migration
+     */
+    public function up() {
+        $this->getSchema()
+            ->create($this->getTableName(), function (Blueprint $table) {
+                $table->increments('id');
+                $table->unsignedTinyInteger('player_id')->references('id')->on('players');
+                $table->unsignedTinyInteger('server_id')->references('id')->on('servers');
+                $table->enum('status', [
+                    'online',
+                    'offline',
+                ])->default('offline');
+                $table->timestamp('disconnected_at')->nullable();
+                $table->timestamps();
+            });
+    }
+    
+    /**
+     * Undo the migration
+     */
+    public function down() {
+        $this->getSchema()->drop($this->getTableName());
+    }
+    
+    /**
+     * @return string
+     */
+    private function getTableName() {
+        return 'players_sessions';
+    }
+}
