744e1874551968c00dbeb8cb54a31838b3c9d082
fant1kua <fant1kua@users.noreply.github.com>
2018-12-27 17:44:15 +0200

    Index admin page

--- src/Controllers/Admin/AdminController.php
+++ src/Controllers/Admin/AdminController.php
@@ -4,6 +4,9 @@ namespace GameX\Controllers\Admin;
 use \GameX\Core\BaseAdminController;
 use \Psr\Http\Message\ServerRequestInterface;
 use \Psr\Http\Message\ResponseInterface;
+use \GameX\Core\Auth\Models\UserModel;
+use \GameX\Models\Server;
+use \GameX\Models\Player;
 
 class AdminController extends BaseAdminController {
 
@@ -17,6 +20,19 @@ class AdminController extends BaseAdminController {
 	public function indexAction(ServerRequestInterface $request, ResponseInterface $response, array $args) {
 //		TODO: For active menu checks
 //		$request->getAttribute('route')->getName();
-		return $this->render('admin/index.twig');
+		return $this->render('admin/index.twig', [
+            'servers' => [
+                'total' => Server::count(),
+                'active' => Server::where('active', 1)->count(),
+            ],
+		    'users' => [
+		        'total' => UserModel::count(),
+                'new' => UserModel::whereRaw('DATE(`created_at`) = CURDATE()')->count()
+            ],
+		    'players' => [
+		        'total' => Player::count(),
+                'new' => Player::whereRaw('DATE(`created_at`) = CURDATE()')->count()
+            ],
+        ]);
 	}
 }
