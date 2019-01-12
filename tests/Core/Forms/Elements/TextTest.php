444a9d00bea40ce0308a61b4f2228928bb88afbc
fant1kua <fant1kua@users.noreply.github.com>
2019-01-13 00:02:39 +0200

    Add text element test

---
+++ tests/Core/Forms/Elements/TextTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use \PHPUnit\Framework\TestCase;
+use \GameX\Core\Forms\Elements\Text;
+
+class TextTest extends TestCase
+{
+    public function testRender() {
+        $element = new Text('testName', 'testValue', [
+            'id' => 'testId',
+            'title' => 'testTitle',
+            'description' => 'testDescription',
+            'required' => true,
+            'classes' => ['testClass'],
+            'attributes' => ['test' => 'TestAttribute']
+        ]);
+        
+        $this->assertEquals('testName', $element->getName());
+        $this->assertEquals('testValue', $element->getValue());
+        $this->assertEquals('testid', $element->getId());
+        $this->assertEquals('testTitle', $element->getTitle());
+        $this->assertEquals('testDescription', $element->getDescription());
+        $this->assertEquals(true, $element->getIsRequired());
+        $this->assertEquals(['testClass'], $element->getClasses());
+        $this->assertEquals(['test' => 'TestAttribute'], $element->getAttributes());
+    }
+}
