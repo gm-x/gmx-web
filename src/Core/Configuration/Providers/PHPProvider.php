9a7f42a20d0df09fb873710e2879dd5a61fed7c2
fant1kua <fant1kua@users.noreply.github.com>
2019-03-25 17:52:45 +0200

    Fix save PHP arrays

--- src/Core/Configuration/Providers/PHPProvider.php
+++ src/Core/Configuration/Providers/PHPProvider.php
@@ -3,6 +3,7 @@ namespace GameX\Core\Configuration\Providers;
 
 use \GameX\Core\Configuration\ProviderInterface;
 use \GameX\Core\Configuration\Node;
+use \GameX\Core\Configuration\Exceptions\CantLoadException;
 use \GameX\Core\Configuration\Exceptions\CantSaveException;
 
 class PHPProvider implements ProviderInterface {
@@ -30,8 +31,8 @@ class PHPProvider implements ProviderInterface {
      * @inheritdoc
      */
     public function load() {
-        $default = require $this->default;
-        $config = require $this->config;
+        $default = $this->loadPHP($this->default);
+        $config = $this->loadPHP($this->config);
 
         return new Node(array_replace_recursive($default, $config));
     }
@@ -44,9 +45,33 @@ class PHPProvider implements ProviderInterface {
             return;
         }
 
-        $data = "<?php\nreturn " . var_export($data->toArray(), true);
+        $data = var_export($data->toArray(), true);
+        $data = preg_replace("/^([ ]*)(.*)/m", '$1$1$2', $data);
+        $array = preg_split("/\r\n|\n|\r/", $data);
+        $array = preg_replace(["/\s*array\s\($/", "/\)(,)?$/", "/\s=>\s$/"], [null, ']$1', ' => ['], $array);
+        $data = join(PHP_EOL, array_filter(["["] + $array));
+        $data = "<?php\nreturn " . $data  . ";\n";
         if (file_put_contents($this->config, $data) === false) {
             throw new CantSaveException('Could not write to file ' . $this->config);
         }
+    }
+
+    /**
+     * @param $path
+     * @return array|mixed
+     * @throws CantLoadException
+     */
+    protected function loadPHP($path) {
+        if (empty($path) || !is_readable($path)) {
+            return [];
+        }
+
+        $data = require $path;
+
+        if (!is_array($data)) {
+            throw new CantLoadException('Bad format of file ' . $path);
+        }
+
+        return $data;
     }
 }
