2298ae562af06725d8c24b98637a2b6f30751e99
fant1kua <fant1kua@users.noreply.github.com>
2019-03-30 00:35:52 +0200

    Permissions for social preferences Save tab in user settings after submit

--- src/Controllers/SettingsController.php
+++ src/Controllers/SettingsController.php
@@ -6,7 +6,6 @@ use \GameX\Core\BaseMainController;
 use \Slim\Http\Request;
 use \Psr\Http\Message\ResponseInterface;
 use \GameX\Constants\SettingsConstants;
-use \GameX\Core\Helpers\UriHelper;
 use \GameX\Core\Auth\Helpers\AuthHelper;
 use \GameX\Forms\Settings\EmailForm;
 use \GameX\Forms\Settings\PasswordForm;
@@ -17,7 +16,7 @@ class SettingsController extends BaseMainController
 {
     protected function getActiveMenu()
     {
-        return SettingsConstants::ROUTE_MAIN;
+        return SettingsConstants::ROUTE_INDEX;
     }
     
     /**
@@ -34,28 +33,36 @@ class SettingsController extends BaseMainController
         $emailForm = new EmailForm($user);
         if ($this->processForm($request, $emailForm, true)) {
             $this->addSuccessMessage($this->getTranslate('labels', 'saved'));
-            return $this->redirect(SettingsConstants::ROUTE_MAIN);
+            return $this->redirect(SettingsConstants::ROUTE_INDEX);
         }
         
         $passwordForm = new PasswordForm($user, new AuthHelper($this->container));
         if ($this->processForm($request, $passwordForm, true)) {
             $this->addSuccessMessage($this->getTranslate('labels', 'saved'));
-            return $this->redirect(SettingsConstants::ROUTE_MAIN, [], ['form' => 'password']);
+            return $this->redirect(SettingsConstants::ROUTE_INDEX, [], ['tab' => 'password']);
         }
         
         $avatarForm = new AvatarForm($user, $this->getContainer('upload'));
+
         if ($this->processForm($request, $avatarForm, true)) {
             $this->addSuccessMessage($this->getTranslate('labels', 'saved'));
-            return $this->redirect(SettingsConstants::ROUTE_MAIN, [], ['form' => 'avatar']);
+            return $this->redirect(SettingsConstants::ROUTE_INDEX, [], ['tab' => 'avatar']);
         }
-        
-        return $this->getView()->render($response, 'settings/main.twig', [
-            'currentHref' => UriHelper::getUrl($request->getUri()),
-            'currentForm' => $request->getParam('form', 'email'),
+
+	    $passwordForm->getForm()->setAction($this->pathFor(SettingsConstants::ROUTE_INDEX, [], ['tab' => 'password']));
+	    $avatarForm->getForm()->setAction($this->pathFor(SettingsConstants::ROUTE_INDEX, [], ['tab' => 'avatar']));
+
+        return $this->getView()->render($response, 'settings/index.twig', [
+            'tab' => $request->getParam('tab', 'email'),
             'user' => $user,
             'emailForm' => $emailForm->getForm(),
             'passwordForm' => $passwordForm->getForm(),
             'avatarForm' => $avatarForm->getForm(),
         ]);
+    }
+
+    protected function getSocialNetworks()
+    {
+    	//
     }
 }
