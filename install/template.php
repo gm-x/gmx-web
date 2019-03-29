2ecfa9c433e5eb2595210d878123a7a2fdecb5b0
fant1kua <fant1kua@users.noreply.github.com>
2019-03-29 21:54:17 +0200

    Fix install

--- install/template.php
+++ install/template.php
@@ -7,8 +7,8 @@
 <head>
     <title>GameX Install</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <link rel="stylesheet" href="../assets/css/uikit.css" />
-    <script src="../assets/js/uikit.js"></script>
+    <link rel="stylesheet" href="assets/css/uikit.css" />
+    <script src="assets/js/uikit.js"></script>
 </head>
 <body>
 <div class="uk-container uk-container-small uk-margin-medium">
@@ -87,7 +87,7 @@
     </form>
 
 </div>
-<script src="../assets/js/jquery-3.3.1.min.js"></script>
+<script src="assets/js/jquery-3.3.1.min.js"></script>
 <script>
     var statusList;
 	function result(message, nextCall) {
@@ -188,7 +188,7 @@
 	function finish() {
         $('#formSubmitButton').prop('disabled', false);
         alert("Successfully installed");
-        location.href = '../';
+        location.href = '/';
 	}
 
     UIkit.util.on('#installForm', 'submit', function (e) {
