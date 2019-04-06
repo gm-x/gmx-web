81f42c1049c196045f60e5f306c8b0bfaba9e9f5
Kruzya <admin@crazyhackgut.ru>
2019-04-06 22:42:49 +0400

    Add grouping for routes in API

--- src/routes/api.php
+++ src/routes/api.php
@@ -4,12 +4,20 @@ use \GameX\Controllers\API\ServerController;
 use \GameX\Controllers\API\PlayerController;
 use \GameX\Controllers\API\PunishController;
 
-$this->post('/server/privileges', BaseController::action(ServerController::class, 'privileges'));
-$this->post('/server/reasons', BaseController::action(ServerController::class, 'reasons'));
-$this->post('/server/info', BaseController::action(ServerController::class, 'info'));
-$this->post('/server/ping', BaseController::action(ServerController::class, 'ping'));
-$this->post('/player/connect', BaseController::action(PlayerController::class, 'connect'));
-$this->post('/player/disconnect', BaseController::action(PlayerController::class, 'disconnect'));
-$this->post('/player/assign', BaseController::action(PlayerController::class, 'assign'));
-$this->post('/punish', BaseController::action(PunishController::class, 'index'));
-$this->post('/punish/immediately', BaseController::action(PunishController::class, 'immediately'));
+$this->group('/server', function() {
+    $this->post('/privileges', BaseController::action(ServerController::class, 'privileges'));
+    $this->post('/reasons', BaseController::action(ServerController::class, 'reasons'));
+    $this->post('/info', BaseController::action(ServerController::class, 'info'));
+    $this->post('/ping', BaseController::action(ServerController::class, 'ping'));
+});
+
+$this->group('/player', function() {
+    $this->post('/connect', BaseController::action(PlayerController::class, 'connect'));
+    $this->post('/disconnect', BaseController::action(PlayerController::class, 'disconnect'));
+    $this->post('/assign', BaseController::action(PlayerController::class, 'assign'));
+});
+
+$this->group('/punish', function() {
+    $this->post('', BaseController::action(PunishController::class, 'index'));
+    $this->post('/immediately', BaseController::action(PunishController::class, 'immediately'));
+});
