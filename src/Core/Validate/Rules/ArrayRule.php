e77e26959fa2186a81125797c19fdea2ed5fa0c7
fant1kua <fant1kua@users.noreply.github.com>
2019-07-01 17:54:06 +0300

    Add sessions filed to ping API action

--- src/Core/Validate/Rules/ArrayRule.php
+++ src/Core/Validate/Rules/ArrayRule.php
@@ -4,41 +4,12 @@ namespace GameX\Core\Validate\Rules;
 class ArrayRule extends BaseRule {
     
     /**
-     * @var integer|null
-     */
-    protected $min;
-    
-    /**
-     * @var integer|null
-     */
-    protected $max;
-    
-    /**
-     * @param integer|null $min
-     * @param integer|null $max
-     */
-    public function __construct($min = null, $max = null) {
-        $this->min = $min;
-        $this->max = $max;
-    }
-    
-    /**
      * @inheritdoc
      */
     public function validate($value, array $values) {
     	if (!is_array($value)) {
-    		return true;
+    		return null;
 	    }
-
-        $count = count($value);
-
-        if ($this->min !== null && $count < $this->min) {
-            return null;
-        }
-    
-        if ($this->max !== null && $count > $this->max) {
-            return null;
-        }
         
         return $value;
     }
@@ -47,14 +18,6 @@ class ArrayRule extends BaseRule {
      * @return array|null
      */
     public function getMessage() {
-        if ($this->min !== null && $this->max !== null) {
-            return ['array_min_max', [$this->min, $this->max]];
-        } elseif ($this->min !== null) {
-            return ['array_min', [$this->min]];
-        } elseif ($this->max !== null) {
-            return ['array_max', [$this->max]];
-        } else {
-            return ['array'];
-        }
+        return ['array'];
     }
 }
